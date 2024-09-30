7c1921344f49a2029ee6d5698f23d22f4a01a51a
dennismuuo044@gmail.com <dennismuuo044@gmail.com>
2024-09-30 17:00:17 +0300

    first commit with my codes

---
+++ mysite/weatherapp/views.py
@@ -0,0 +1,33 @@
+import urllib
+import json
+
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def index(request):
+
+    if request.method == 'POST':
+        city = request.POST['city']
+
+        source = urllib.request.urlopen('<YOUR API GOES HERE>').read()
+        list_of_data = json.loads(source)
+
+        data = {
+            "country_code": str(list_of_data['sys']['country']),
+            "coordinate": str(list_of_data['coord']['lon']) + ', '
+            + str(list_of_data['coord']['lat']),
+
+            "temp": str(list_of_data['main']['temp']) + ' °C',
+            "pressure": str(list_of_data['main']['pressure']),
+            "humidity": str(list_of_data['main']['humidity']),
+            'main': str(list_of_data['weather'][0]['main']),
+            'description': str(list_of_data['weather'][0]['description']),
+            'icon': list_of_data['weather'][0]['icon'],
+        }
+        print(data)
+    else:
+        data = {}
+
+    return render(request, "main/index.html", data)
